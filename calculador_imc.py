756de93aea8c8187c42ecf05bbb62790976b7e67
Cido <ajfranceschini2015@gmail.com>
2025-06-02 11:59:21 -0300

    calculadora imc

---
+++ calculador_imc.py
@@ -0,0 +1,46 @@
+nome = input("digite seu nome: ")
+peso = float(input("digite seu peso: "))
+altura = float(input("qual sua altura: "))
+
+imc = peso/(altura*altura)
+ 
+if imc < 18.5:
+    print (f"Ola {nome}, seu imc é {imc}, sua classificação e obsidade 0")
+
+elif imc > 18.5 and imc <= 24.9:
+    print (f"Ola {nome}, seu imc é {imc}, sua classificação e normal 0")
+
+elif imc > 25.0 and imc <= 29.9:
+    print (f"Ola {nome}, seu imc é {imc}, sua classificação e sobrepeso 1")
+
+elif imc > 30.0 and imc <=  39.9:
+    print (f"Ola {nome}, seu imc é {imc}, sua classificação e obsidade 2")
+
+elif imc >= 40.0:
+    print (f"Ola {nome}, seu imc é {imc}, sua classificação e obsidade grave 2")
+
+
+
+
+# 2) Atividade: Calculadora de IMC com Classificação e
+# Comparações
+# 🎯 Objetivo:
+# Criar um programa que:
+# ● Solicite dados do usuário.
+# ● Calcule o IMC (Índice de Massa Corporal).
+# ● Classifique o resultado usando operadores e tipos de dados.
+# ● Mostre comparações de forma booleana.
+# Veja a interpretação do IMC
+
+
+# Menor que 18,5 Magreza 0
+# Entre 18,5 e 24,9 Normal 0
+# Entre 25,0 e 29,9 Sobrepeso I
+# Entre 30,0 e 39,9 Obesidade II
+# Maior que 40,0 Obesidade Grave III
+
+
+
+# 3) Atividade: Caixa de Supermercado com Desconto
+# Criar um programa que simule o caixa de um supermercado, aplicando um desconto para
+# compras acima de R$ 100,00.
